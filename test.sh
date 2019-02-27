abe7f572b40090d3d2028c5988fc14e5b1ddcd4a
Matt Bangert <matt.bangert@diio.net>
2019-02-27 13:04:47 -0700

    Bring up to date.

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Author: Matt Bangert <matt.bangert@cirium.com>
+set -e
+set -o pipefail
+# get the dockerfiles changed
+IFS=$'\n'
+# shellcheck disable=SC2207
+files=( $(git diff --name-only master | cut -d ' ' -f2 | grep 'Dockerfile') )
+unset IFS
+
+# build the changed dockerfiles
+# shellcheck disable=SC2068
+for f in ${files[@]}; do
+	if ! [[ -e "$f" ]]; then
+		continue
+	fi
+
+	(
+	set -x
+    echo -e "Linting $f"
+	docker run --rm -i hadolint/hadolint < "$f"
+	)
+done
